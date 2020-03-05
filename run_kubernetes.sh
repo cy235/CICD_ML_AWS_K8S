a1f831f95c34aae8357e771a2892b3824e904657
cy235 <44735485+cy235@users.noreply.github.com>
2020-03-05 10:41:39 -0500

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,11 +5,11 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=aminueza/udacity-prediction:v0.1
+dockerpath=chenyi/cy235-prediction:v0.1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-kubectl run udacity-prediction --port=80 --image=$dockerpath
+kubectl run cy235-prediction --port=80 --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
@@ -17,4 +17,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward $(kubectl get pods -o=name | grep udacity-prediction | sed "s/^.\{4\}//") 8000:80
+kubectl port-forward $(kubectl get pods -o=name | grep cy235-prediction | sed "s/^.\{4\}//") 8000:80
