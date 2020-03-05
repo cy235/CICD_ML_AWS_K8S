3c3c025a3e316f30fbe6363faddc6a5eb21f3cd6
cy235 <44735485+cy235@users.noreply.github.com>
2020-03-05 10:35:43 -0500

    Update udacity_prediction.py

--- tests/udacity_prediction.py
+++ tests/udacity_prediction.py
@@ -1,11 +1,11 @@
-from testscase import test_udacity
+from testscase import test_cy235
 
 
 def test_main():
-    result = test_udacity.call_web_service()
+    result = test_cy235.call_web_service()
     assert result.status_code == 200
 
 
 def test_predict():
-    result = test_udacity.post_music()
+    result = test_cy235.post_music()
     assert result.status_code == 200
