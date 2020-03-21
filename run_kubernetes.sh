15f9977aad0e7b0b8a1aa0d7a8d4766af843c7bc
cy235 <44735485+cy235@users.noreply.github.com>
2020-03-20 21:10:30 -0400

    Update run_kubernetes.sh

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,7 +5,7 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath=cy235/cy235-prediction:v0.1
+dockerpath=index.docker.io/cy235/cy235-prediction:v1
 
 # Step 2
 # Run the Docker Hub container with kubernetes
